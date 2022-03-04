7b5aef3fe2046f5e4664921ecaafc04dada5361b
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2022-03-04 21:28:45 +0000

    v0.1

--- slap.py
+++ slap.py
@@ -1,17 +1,28 @@
-__module_name__ = 'Slapper'
-__module_version__ = '0.0.1'
-__module_description__ = 'Slapper'
-__author__ = 'ComputerTech'
+# -*- coding: UTF-8 -*-
+
+from random import choice
 
 import hexchat
 
-def slapper(word, word_eol, userdata):
+__module_name__ = 'Slapper'
+__module_version__ = '0.1'
+__module_description__ = 'A Slap plugin for HexChat'
+__module_author__ = 'ComputerTech'
+ 
+slaps = [
+    'slaps {} around a bit with a large trout',
+    'slaps {} around a bit with a extra large dildo',
+    'slaps {} around a bit with a large wet towel',
+    'slaps {} around a bit with a large beer',
+    'slaps {} around a bit with a small pussy',
+]
 
-
-        hexchat.command("me slaps \002{}\002 around a bit with a large trout.".format(word[1]))
-
-        return hexchat.EAT_ALL
-
-
-hexchat.hook_command('slap',slapper,)
-
+def slap(word, word_eol, userdata):
+    if len(word) > 1:
+        hexchat.command('me ' + choice(slaps).format(word[1]))
+    else:
+        hexchat.command('help slap')
+    return hexchat.EAT_ALL
+ 
+hexchat.hook_command('slap', slap, help="Syntax: /slap <nick>")
+hexchat.prnt(__module_name__ + " loaded.")
