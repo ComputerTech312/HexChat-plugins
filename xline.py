3ac7ed072e5118dcbadfff34de6b2ea0199f4b30
Colby <60820431+ComputerTech312@users.noreply.github.com>
2022-07-27 19:41:59 +0100

    update xline.py

--- xline.py
+++ xline.py
@@ -1,1 +1,13 @@
+import hexchat
 
+__module_name__ = 'Xline'
+__module_author__ = 'ComputerTech'
+__module_version__ = '0.1'
+__module_description__ = 'A simple Gline script.'
+
+def ctgline(word, word_eol, userdata):
+  target = word[1]
+  hexchat.command(f'GZLINE {target} 1d Banned')
+  
+hexchat.hook_command('xline', icycle)
+hexchat.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded')
