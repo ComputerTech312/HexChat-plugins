dc6736629e74dfa5eeeb44ffca711ed9ef0482a9
Colby <60820431+ComputerTech312@users.noreply.github.com>
2022-07-29 19:32:00 +0100

    Update updown.py

--- updown.py
+++ updown.py
@@ -1,47 +1,25 @@
-# -*- coding: UTF-8 -*-
-
 import hexchat
 
-__module_name__ = "updown.py"
-
-__module_version__ = "0.0.1"
-
-__module_author__ = "ComputerTech"
-
-__module_description__ = "Manage your access on the current channel"
+__module_name__ = 'Zap'
+__module_author__ = 'ComputerTech'
+__module_version__ = '0.1'
+__module_description__ = 'ZAP!'
 
 def isop():
-
-	me = hexchat.get_info("nick")	for u in hexchat.get_list("users"):
-
-		if u.nick == me:
-
+	me = hexchat.get_info("nick")
+	for people in hexchat.get_list("users"):
+		if people.nick == me:
 			if '@' in u.prefix:
-
 				return True
-
 			else:
-
 				return False
-
-def up(word, word_eol, userdata):
-
-	chan = hexchat.get_info('channel')
-
-	if not chan.startswith('#'):
-
-		print("This command must be used in a channel window.")
-
-		return hexchat.EAT_ALL
-
+    
+def oop(word, word_eol, userdata):
+	chan = hexchat.get_info("channel")
 	if isop():
-
-		print("You're already Op'd in", chan)
-
+		print("You're already op'd in", chan)
 		return hexchat.EAT_ALL
-
-	hexchat.command("msg chanserv OP", chan)
-
-hexchat.hook_command('up', up, help="Ops you with chanserv in the current channel, if you are not already.")
-
-hexchat.hook_command('down', down, help="Ops you with chanserv in the current channel, if you are not already.")
+	hexchat.command("cs OP " + chan)
+  
+hexchat.hook_command('oop', oop, help="Gives you Op via chanserv")
+hexchat.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded')
