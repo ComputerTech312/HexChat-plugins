a225ea44926a3f9f13e7acc87fd5b776661edfa0
Colby <60820431+ComputerTech312@users.noreply.github.com>
2022-07-30 20:01:38 +0100

    Update xline.py

--- xline.py
+++ xline.py
@@ -7,8 +7,7 @@ __module_description__ = 'A simple Network ban script.'
 
 def xline(word, word_eol, userdata):
   target = word[1]
-  reason = word.split(' ',1)[2]
-  hexchat.command(f'GZLINE {target} 1d {reason}')
+  hexchat.command(f'GZLINE {target} 1d Change reason here')
   
 hexchat.hook_command('xline', xline)
 hexchat.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded')
